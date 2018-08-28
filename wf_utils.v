b8e4d8c90752f39f50ad066063d2ce3f4e98a614
Dominique Larchey-Wendling <larchey@gmail.com>
2018-08-29 01:23:16 +0200

    cosmetic changes

--- wf_utils.v
+++ wf_utils.v
@@ -13,16 +13,18 @@ Set Implicit Arguments.
 
 Section measure_rect.
 
-  Variable (X : Type) (m : X -> nat) (P : X -> Type)
-           (HP : forall x, (forall y, m y < m x -> P y) -> P x).
+  Variable (X : Type) (m : X -> nat) (P : X -> Type).
 
   Let R x y := m x < m y.
 
   Fact Acc_measure : well_founded R.
   Proof. unfold R; apply wf_inverse_image, lt_wf. Qed.
 
-  Theorem measure_rect x : P x.
+  Theorem measure_rect : 
+        (forall x, (forall y, m y < m x -> P y) -> P x) 
+     -> (forall x,                                 P x).
   Proof.
+    intros HP x.
     cut (Acc R x).
     + revert x.
       exact (fix loop x Hx { struct Hx } := @HP x (fun y Hy => loop y (Acc_inv Hx Hy))).
@@ -33,13 +35,15 @@ End measure_rect.
 
 Section measure_double_rect.
 
-  Variable (X Y : Type) (m : X -> Y -> nat) (P : X -> Y -> Type)
-           (HP : forall x y, (forall x' y', m x' y' < m x y -> P x' y') -> P x y).
+  Variable (X Y : Type) (m : X -> Y -> nat) (P : X -> Y -> Type).
 
   Let R c d := m (fst c) (snd c) < m (fst d) (snd d).
 
-  Theorem measure_double_rect x y : P x y.
+  Theorem measure_double_rect : 
+        (forall x y, (forall x' y', m x' y' < m x y -> P x' y') -> P x y)
+     -> (forall x y,                                               P x y).
   Proof.
+    intros HP x y.
     cut (Acc R (x,y)).
     + revert x y.
       refine (fix loop x y H { struct H } := @HP x y (fun x' y' H' => loop x' y' (Acc_inv H _))).
