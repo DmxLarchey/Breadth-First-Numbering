c44898ca231ccd06ba7fc07a74ea3077b5736100
Dominique Larchey-Wendling <larchey@wendling>
2018-09-14 11:53:44 +0200

    better separation between CC and PO in bfr_fifo_3q_full.v

--- bfr_fifo_3q_full.v
+++ bfr_fifo_3q_full.v
@@ -35,44 +35,59 @@ Section bfr_3q.
          -> { q : fY | fifo_3q_list p ~lt rev (fifo_3q_list q) 
                     /\ bft_f (rev (fifo_3q_list q)) = ll }.
   Proof.
-    intros Hll.
-    unfold fifo_3q_sum in Hll. 
-    destruct ll as [ | y ll ].
-    { refine(let (q,Hq) := @fifo_3q_nil_full _ in exist _ q _).
-      revert Hll; rewrite Hq; simpl.
+    unfold fifo_3q_sum in *.
+    refine (match ll with 
+      | nil   => fun Hll => let (q,Hq)   := @fifo_3q_nil_full _ 
+                            in  exist _ q _
+      | y::mm => fun Hll => let (d1,Hd1) := fifo_3q_deq_full p _ 
+                            in  _
+    end).
+    all: cycle 2.
+    revert Hd1; refine (match d1 with (t,p1) => _ end); intros Hp1.
+    rewrite Hp1 in Hll; simpl in Hll; clear d1.
+    revert Hll Hp1; refine (match t with 
+      | leaf x     => fun Hll Hp1 => let (q,Hq)   := bfr_3q_f p1 mm _ in 
+                                     let (q1,Hq1) := fifo_3q_enq_full q (leaf y) 
+                                     in  exist _ q1 _
+      | node a x b => fun Hll Hp1 => let (p2,Hp2) := fifo_3q_enq_full p1 a    in
+                                     let (p3,Hp3) := fifo_3q_enq_full p2 b    in
+                                     let (q,Hq)   := bfr_3q_f p3 mm _         in  
+                                     let (d2,Hd2) := fifo_3q_deq_full q _     in _
+    end); auto.
+    all: cycle 3.
+    revert Hd2; refine (match d2 with 
+      | (u,q1) => fun Hd2 => let (d3,Hd3) := fifo_3q_deq_full q1 _ 
+                             in  _ 
+    end).
+    all: cycle 1.
+    revert Hd3; refine (match d3 with
+      | (v,q2) => fun Hd3 => let (q3,Hq3) := fifo_3q_enq_full q2 (node v y u) 
+                             in  exist _ q3 _
+    end).
+    all: cycle 1.
+
+    * revert Hll; rewrite Hq; simpl.
       generalize (fifo_3q_list p).
       intros [ | [] ? ]; simpl; try discriminate.
       split; auto.
-      rewrite bft_f_fix_0; auto. }
-    refine (let (d1,Hd1) := fifo_3q_deq_full p _ in _).
-    { intros E; rewrite E in Hll; discriminate. }
-    revert d1 Hd1; intros (x,p1) Hp1.
-    rewrite Hp1 in Hll; simpl in Hll.
-    destruct x as [ x | a x b ]. 
-    + refine (let (q,Hq) := bfr_3q_f p1 ll _ in _); auto.
-      destruct Hq as (Hq1 & Hq2).
-      refine (let (q1,Hq1) := fifo_3q_enq_full q (leaf y) in exist _ q1 _).
+      rewrite bft_f_fix_0; auto.
+    * intros E; rewrite E in Hll; discriminate.
+    * destruct Hq as (Hq2 & Hq3).
       rewrite Hp1, Hq1, rev_app_distr; split; simpl; auto.
-      rewrite bft_f_fix_3, <-app_nil_end, Hq2; auto.
-    + refine (let (p2,Hp2) := fifo_3q_enq_full p1 a    in
-              let (p3,Hp3) := fifo_3q_enq_full p2 b    in
-              let (q,Hq)   := bfr_3q_f p3 ll _
-              in _).
-      { unfold fifo_3q_sum. rewrite Hp3, Hp2.
-        repeat rewrite lsum_app; simpl in Hll |- *; omega. }
-      destruct Hq as (Hq_1 & Hq_2).
-      apply Forall2_rev in Hq_1.
-      rewrite rev_involutive in Hq_1.
-      refine (let (d2,Hd2) := fifo_3q_deq_full q _ in _).
-      { intros H; rewrite H, Hp3, rev_app_distr in Hq_1.
-        inversion Hq_1. }
-      destruct d2 as (u,q1).
-      refine (let (d3,Hd3) := fifo_3q_deq_full q1 _ in _).
-      { intros H; rewrite Hd2, H, Hp3, Hp2 in Hq_1. 
-        repeat rewrite rev_app_distr in Hq_1.
-        inversion Hq_1. inversion H5. }
-      destruct d3 as (v,q2).
-      refine(let (q3,Hq3) := fifo_3q_enq_full q2 (node v y u) in  exist _ q3 _).
+      rewrite bft_f_fix_3, <- app_nil_end, Hq3; auto.
+    * rewrite Hp3, Hp2; repeat rewrite lsum_app; simpl in Hll |- *; omega.
+    * destruct Hq as (Hq_1 & Hq_2); 
+      apply Forall2_rev in Hq_1; rewrite rev_involutive in Hq_1.
+      intros H; rewrite H, Hp3, rev_app_distr in Hq_1; inversion Hq_1.
+    * destruct Hq as (Hq_1 & Hq_2).
+      apply Forall2_rev in Hq_1; rewrite rev_involutive in Hq_1.
+      intros H; rewrite Hd2, H, Hp3, Hp2 in Hq_1. 
+      repeat rewrite rev_app_distr in Hq_1.
+      inversion Hq_1.
+      inversion H5.
+    * destruct Hq as (Hq_1 & Hq_2).
+      apply Forall2_rev in Hq_1; rewrite rev_involutive in Hq_1.
+      simpl in *.
       rewrite Hp1, Hq3, rev_app_distr; simpl.
       rewrite Hp3, Hp2, Hd2, Hd3 in Hq_1.
       repeat rewrite rev_app_distr in Hq_1.
@@ -95,22 +110,26 @@ Section bfr_3q.
       refine (match @bfr_3q_f (fifo_3q_enq fifo_3q_nil t) l _ with 
         | exist _ q Hq => let (d1,Hd1) := fifo_3q_deq_full q _ in _ 
       end).
-      { unfold fifo_3q_sum; rewrite fifo_3q_enq_spec, fifo_3q_nil_spec; simpl; omega. }
-      { intros E; rewrite E in Hq; apply proj2 in Hq; simpl in Hq.
+      all: cycle 2.
+      revert Hd1; refine(match d1 with
+        | (t',q') => fun Hd1 => exist _ t' _
+      end).
+      all: cycle 1.
+
+      + unfold fifo_3q_sum; rewrite fifo_3q_enq_spec, fifo_3q_nil_spec; simpl; omega.
+      + intros E; rewrite E in Hq; apply proj2 in Hq; simpl in Hq.
         rewrite bft_f_fix_0 in Hq; subst.
-        generalize (m_bt_ge_1 t); simpl in *; omega. }
-      destruct Hq as (Hq_1 & Hq_2).
-      destruct d1 as (t',q').
-      exists t'.
-      apply Forall2_rev in Hq_1.
-      rewrite fifo_3q_enq_spec, fifo_3q_nil_spec, rev_involutive, Hd1 in Hq_1; simpl in Hq_1.
-      apply Forall2_cons_inv in Hq_1.
-      destruct Hq_1 as (Hq_3 & Hq_1).
-      apply Forall2_nil_inv_right in Hq_1.
-      split; auto.
-      rewrite Hd1, Hq_1 in Hq_2.
-      simpl in Hq_2.
-      rewrite <- bft_std_eq_bft; auto.
+        generalize (m_bt_ge_1 t); simpl in *; omega.
+      + destruct Hq as (Hq_1 & Hq_2); simpl in Hd1.
+        apply Forall2_rev in Hq_1.
+        rewrite fifo_3q_enq_spec, fifo_3q_nil_spec, rev_involutive, Hd1 in Hq_1; simpl in Hq_1.
+        apply Forall2_cons_inv in Hq_1.
+        destruct Hq_1 as (Hq_3 & Hq_1).
+        apply Forall2_nil_inv_right in Hq_1.
+        split; auto.
+        rewrite Hd1, Hq_1 in Hq_2.
+        simpl in Hq_2.
+        rewrite <- bft_std_eq_bft; auto.
     Qed.
 
     Definition bfr_3q t l H := proj1_sig (bfr_3q_full t l H).
