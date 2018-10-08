8240bf12a30b1986700b5c21994fe1b225faaacd
Ralph Matthes <matthes@irit.fr>
2018-10-08 23:19:57 +0200

    a possible organization of the extraction with all four FIFO implementations

--- benchmarks.v
+++ benchmarks.v
@@ -27,11 +27,21 @@ Extract Inductive prod => "(*)"  [ "(,)" ].
 Extract Inductive list => "list" [ "[]" "(::)" ].
 Extract Inductive nat => int [ "0" "succ" ] "(fun fO fS n -> if n=0 then fO () else fS (n-1))".
 
-(* Depending on the content of fifo.v, the extraction here provides either
+Module BFN_triv := BFN_FIFO FIFO_triv.
+Module BFN_2lists := BFN_FIFO FIFO_2lists.
+Module BFN_3llists := BFN_FIFO FIFO_3llists.
+(** the following does not yield executable extracted programs *)
+Module BFN_axm := BFN_FIFO FIFO_axm.
+
+
+(* The extraction here provides either
    1) an empty implementation of FIFOs (with fifo_axm)
    2) a trivial implementation with lists (with fifo_triv)
    3) an implementation with 2 lists and efficient reverse (with fifo_2lists), (relaxed) O(1) enqueue/dequeue
    4) an implementation with 3 lazy lists (with fifo_3llists), O(1) enqueue/dequeue
  *)
 
-Extraction "bfn" bfn_fifo.bfn_fifo.
+Extraction "bfn_axm" BFN_axm.bfn_fifo.
+Extraction "bfn_triv" BFN_triv.bfn_fifo.
+Extraction "bfn_2lists" BFN_2lists.bfn_fifo.
+Extraction "bfn_3llists" BFN_3llists.bfn_fifo.
